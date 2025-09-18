1af31abea2e7477c63887c8939908f85983b116c
Bakaloneking <bakaloneking@hotmail.com>
2025-09-18 12:57:12 -0400

    Adicionando algumas coisas relacionadas a primeira tela

--- app/src/main/java/com/example/socorristajunior/ui/theme/Color.kt
+++ app/src/main/java/com/example/socorristajunior/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.example.socorristajunior.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Cores escolhidas pelo Gustavo
+// Adicionei um "s" no começo de cada cor para diferenciar das cores que vem por padrão
+
+val sRed = Color(0xFFE62727)
+
+val sWhite = Color(0xFFF3F2EC)
+
+val sWhiteGrey = Color(0xFFDCDCDC)
+
+val sOceanBlue = Color(0xFF1E93AB)
+
+// Cores que vem por padrão
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
