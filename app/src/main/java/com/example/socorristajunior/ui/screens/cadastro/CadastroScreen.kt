38e26fa1b89c3abb3ca5f5ce4dc2d31a422b84b6
Bakaloneking <bakaloneking@hotmail.com>
2025-11-13 14:48:18 -0400

    Pequeno ajuste de arrowback no cadatro.

--- app/src/main/java/com/example/socorristajunior/ui/screens/cadastro/CadastroScreen.kt
+++ app/src/main/java/com/example/socorristajunior/ui/screens/cadastro/CadastroScreen.kt
@@ -2,6 +2,8 @@ package com.example.socorristajunior.ui.screens.cadastro
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -51,7 +53,14 @@ fun CadastroScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Novo Cadastro") })
+            TopAppBar(
+                title = { Text("Novo Cadastro") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Voltar para Home")
+                    }
+                }
+                )
         }
     ) { paddingValues ->
         Column(
